8d7b42e66875ea9eec42a137166549ab869b3073
Alex Oshiro <alexxoshiro@gmail.com>
2014-09-17 08:01:13 -0400

    Revert "00"
    This reverts commit 846576625cf8430cfcc6663e39429dc7c28008f4.

--- main.cpp
+++ main.cpp
@@ -1,36 +1,13 @@
 #include <gl/glut.h>
-
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <string>
 #include <iostream>
 #include <sstream>
 
-#include <vector>
-
-// ESTRUTURAS, TIPOS CUSTOMIZADOS
-struct Campo
-{
-    bool campo_mina;            // Campo contém uma mina ?
-    bool revelado;              // Campo revalado ?
-    int minas_adja;             // Quantidade de Minas Adjacentes
-    int pos_x;                  // Posição X do Campo
-    int pos_y;                  // Posição Y do Campo
-};
-
 // VARIAVEIS GLOBAIS
-int G_linhas = 0;
-int G_colunas = 0;
-
-int G_pos_x = 0;
-int G_pos_y = 0;
-
 int timer = 0;
 
-std::vector<Campo> campo_minado;                                            // Similar a um ArrayList [Java] para representar o Campo Minado
-
-// FUNÇÕES
 template <typename T>
 std::string to_string(T value)//função to_string criada na mão
 {
@@ -69,7 +46,6 @@ void mostraTempo(int value){ //Função para mostrar o tempo decorrido
     glutTimerFunc(1000,mostraTempo, 1);
 
 }
-
 void acrescentaMarcacao(const char *opcao, int x, int y){
     std::string text;
     text = opcao; //Converte o timer para uma string
@@ -78,8 +54,6 @@ void acrescentaMarcacao(const char *opcao, int x, int y){
     renderText(text.data(), text.size(), x, y);
 
 }
-
-// VERIFICAR SE IRÁ SER MANTIDO
 static void MenuPrincipal(int operador)
 {
     switch( operador )
@@ -95,7 +69,6 @@ static void MenuPrincipal(int operador)
     glutPostRedisplay();
 }
 
-// VERIFICAR SE IRÁ SER MANTIDO
 static void CriaMenus()
 {
     int menu;
@@ -107,114 +80,58 @@ static void CriaMenus()
     glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
-static void Quadrado()         // Com o atual ViewPort, gerará um quadrado com 70 x 70 pixels
+
+static void Quadrado()
 {
     glBegin(GL_LINE_LOOP);
-        glVertex2i(0, 0);
-        glVertex2i(0, 1);
-        glVertex2i(1, 1);
-        glVertex2i(1, 0);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 1, 0);
+        glVertex3f(1, 1, 0);
+        glVertex3f(1, 0, 0);
     glEnd();
 }
 
-static void Revelado()           // Com o atual ViewPort, gerará um quadrado com 70 x 70 pixels
-{
-    glBegin(GL_QUADS);
-        glVertex2i(0, 0);
-        glVertex2i(0, 1);
-        glVertex2i(1, 1);
-        glVertex2i(1, 0);
-    glEnd();
-}
-
-static void Tabuleiro(int linhas, int colunas, int pos_x, int pos_y)
+static void Tabuleiro(int linhas, int colunas)
 {
     int linha = 0;
     int coluna = 0;
-
     for(linha = 0; linha < linhas; linha++)
     {
         for(coluna = 0; coluna < colunas; coluna++)
         {
             //printf("x = %d, y = %d\n",linha ,coluna); //Printa no console as posições desenhadas
-            //printf("\nPos_x: %d Pos_y: %d\n", pos_x, pos_y);
-            if(pos_x > 0 && pos_y > 0)
-            {
-                if((pos_x >= linha * 70) &&
-                   (pos_x <  linha * 70 + 70))
-                {
-                    if(pos_y >= coluna * 70 &&
-                        pos_y <  coluna * 70 + 70)
-                    {
-                        printf("\nRevelar Quadrado X: %d, Y: %d\n", linha, coluna);
-                        glPushMatrix();
-                            glTranslated(linha, coluna, 0);
-                            glColor3f(0.0f, 0.0f, 1.0f);
-                            Revelado();
-                        glPopMatrix();
-                        G_pos_x = 0;
-                        G_pos_y = 0;
-                    }
-
-                    else
-                    {
-                        glPushMatrix();
-                        glTranslated(linha, coluna, 0);
-                        glColor3f(0.0f, 0.0f, 1.0f);
-                        Quadrado();
-                        glPopMatrix();
-                    }
-                }
-
-                else
-                {
-                    glPushMatrix();
-                    glTranslated(linha, coluna, 0);
-                    glColor3f(0.0f, 0.0f, 1.0f);
-                    Quadrado();
-                    glPopMatrix();
-                }
-            }
-            else
-            {
-                glPushMatrix();
-                glTranslated(linha, coluna, 0);
-                glColor3f(0.0f, 0.0f, 1.0f);
-                Quadrado();
-                glPopMatrix();
-            }
+            glPushMatrix();
+            glTranslatef(linha, coluna, 0);
+            glColor3f(0.0f, 0.0f, 1.0f);
+            Quadrado();
+            glPopMatrix();
         }
     }
-
     int valor = 0;
+
 };
 
 void Atualiza_tamanho(int largura, int altura)
 {
-    glClearColor(0.0,0.0,0.0,0);
-    glLoadIdentity();
-    glMatrixMode(GL_PROJECTION);
-    glOrtho(0.0,100.0,0.0,100.0,-100.0,100.0);
-    glMatrixMode(GL_MODELVIEW);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-glFlush();
+    glViewport(0, 0, largura, altura);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+    //gluOrtho2D (-10.0f, 10.0f, -10.0f, 10.0f);
+    gluOrtho2D (0.0f, 10.0f, -2.0f, 10.0f);
 
     printf("\n[DEBUG] : Evento Atualiza tamanho\n");
 }
 
 static void Atualiza_desenho(void)
 {
-    int linha = 11;     // Inicia variaveis para linhas correspondentes ao tabuleiro
-    int coluna = 11;    // Inicia variaveis para colunas correspondentes ao tabuleiro
-    int campo[100];     // Era para ser um vetor utilizado como cada quadrado da tabela
+    int linha = 11;
+    int coluna = 11; //Inicia variaveis para linha e coluna correspondentes ao tabuleiro
+    int campo[100]; //Era para ser um vetor utilizado como cada quadrado da tabela
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    glPushMatrix();
-        Tabuleiro(linha, coluna, G_pos_x, G_pos_y);
-    glPopMatrix();
+    Tabuleiro(linha, coluna);
 
     glPushMatrix();//Quadrado para tempo
         glTranslatef(1.0, -1.1, 0);
@@ -261,8 +178,6 @@ void mouse(int botao, int estado, int x, int y)
         case GLUT_LEFT_BUTTON:
             acrescentaMarcacao("teste", x, y);
             printf("\n X: %d,  Y:  %d\n", x, y);
-            G_pos_x = x;
-            G_pos_y = y;
             glutPostRedisplay();
             break;
         case GLUT_RIGHT_BUTTON:
