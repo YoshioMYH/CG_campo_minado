20dc64a9b9a8c62e17d0fb8e4060ca20febac3c6
Marcelo <marcelomyh1011@gmail.com>
2014-09-18 22:14:38 -0400

    20v2
    Arrumado alguns comentarios
    Uma ou outra optimizacao

--- main.cpp
+++ main.cpp
@@ -22,19 +22,22 @@ GLint windowsSize_x = 600;      // Largura da janela
 GLint windowsSize_y = 600;      // Altura da janela
 
 int G_minas = 0;                // Quantidade de minas no campo
-int G_minasatuais;
+int G_minasatuais = 0;
+
+int G_bandeiras = 0;            // Quantidade de bandeiras no jogo. No Bandeiras = No Minas
+int G_bandeiras_atuais = 0;     // Quantidade de bandeiras colocados no tabuleiro
+
 int G_linhas = 0;               // Quantidade de Linhas do tabuleiro
 int G_colunas = 0;              // Quantidade de Colunas do tabuleiro
 
-int G_click_pos_x = 0.0;          // Posicao X do clique do mouse
-int G_click_pos_y = 0.0;          // Posicao Y do clique do mouse
+int G_click_pos_x = 0.0;        // Posicao X do clique do mouse
+int G_click_pos_y = 0.0;        // Posicao Y do clique do mouse
 int G_operacao_desenho = 0;     // Determina qual operacao o desenho deve fazer
 /*
 0 = nao modifica tabuleiro [nao possivel via E/S]
 1 = revelar um campo [Mouse LB]
 2 = posicionar bandeira [Mouse RB]
 */
-int posicaoVector;
 
 int G_timer = 0;
 int G_dificuldade = 0;
@@ -49,44 +52,47 @@ std::string to_string(T value){                                         //função
     return os.str() ;
 }
 
-static void iniciaCampos();                                             //
-static void AcrescentaMina();                                           //
+static void iniciaCampos();                                             // Funcionando
+static void AcrescentaMina();                                           // Funcionando
 static void CampoMinasAdjacentes();                                     //
-static void MenuTemporario();                                           //
+static void MenuTemporario();                                           // Funcionando [Melhorar]
 static void renderText(const char *text, int length, int x, int y);     // Funcionando
 static void mostraTempo(int value);                                     // Funcionando
 static void mostraMinas();                                              // Funcionando
 static void acrescentaMarcacao(const char *opcao, int x, int y);        // não funcionando, estou procurando arrumar - By Alex
-static void Quadrado();                                                 // Funcionando
-static void Revelado();                                                 // Funcionando
-static void Bandeira();                                                 //
 
-static void Tabuleiro();                                                // Funcionando [Incrementar]
-static void Calculo_Desenho(int linha, int coluna, int indice);         //
-static bool Calculo_Posicao(int linha, int coluna);                     //
-static void Revelar_Campo(int indice);                                  //
-static void Alternar_Protecao(int indice);                              //
+static void Quadrado();                                                 // Funcionando [Melhorar]
+static void Revelado();                                                 // Funcionando [Melhorar]
+static void Bandeira();                                                 // Funcionando [Melhorar]
+static void Tabuleiro();                                                // Funcionando
+
+static void Calculo_Desenho(int linha, int coluna, int indice);         // Funcionando
+static bool Calculo_Posicao(int linha, int coluna);                     // Funcionando
+static void Revelar_Campo(int indice);                                  // Funcionando [Melhorar]
+static void Alternar_Protecao(int indice);                              // Funcionando
 
 static void Atualiza_tamanho(int largura, int altura);                  // Funcionando
-static void Atualiza_desenho(void);                                     // Funcionando [Incrementar]
+static void Atualiza_desenho(void);                                     // Funcionando [Melhorar]
 
 static void teclado(unsigned char tecla, int x, int y);                 // Funcionando
-static void mouse(int botao, int estado, int x, int y);                 // Funcionando [Incrementar]
-static void Limpa();
+static void mouse(int botao, int estado, int x, int y);                 // Funcionando
 
-static void iniciaCampos(){
+static void Limpa();                                                    //
+
+static void iniciaCampos()
+{// Funcao que inicializa todos os campos com valores nulos
     int i = 0;
     int j = 0;
     int cont = 0;
-    for(i = 0; i < G_linhas; i++){                                 //Inicia todos os campos com nada
+    for(i = 0; i < G_linhas; i++){
         for(j = 0; j < G_colunas; j++){
             campo_minado.push_back(Campo());
             campo_minado[cont].campo_mina = false;
             campo_minado[cont].revelado = false;
             campo_minado[cont].protegido = false;
             campo_minado[cont].minas_adja = 0;
-            campo_minado[cont].pos_x = i;                   //Arrumar para marcar corretamente
-            campo_minado[cont].pos_y = j;                   //Arrumar para marcar corretamente
+            campo_minado[cont].pos_x = i;
+            campo_minado[cont].pos_y = j;
             printf("\nCampo: %2d  Valor: %2d  PosX: %2d  PosY: %2d", cont, campo_minado[cont].minas_adja, campo_minado[cont].pos_x, campo_minado[cont].pos_y);
             cont++;
         }
@@ -94,7 +100,8 @@ static void iniciaCampos(){
 
 }
 
-static void  AcrescentaMina(){
+static void  AcrescentaMina()
+{// Funcao para gerar as posicoes das Minas no Tabuleiro
     srand(time(NULL));
     int i = 0;
     int minimo = 0;
@@ -113,23 +120,24 @@ static void  AcrescentaMina(){
     }
 }
 
-void MenuTemporario(){
+static void MenuTemporario()
+{// Menu para selecionar o nível de dificuldade
     printf("Iniciando Menu Temporario \nEscolha a dificuldade(0-Noob, 1-Menos Noob, 2-SabeUmPouco): \n");
     scanf("%d", &G_dificuldade);
     switch(G_dificuldade){
-        case 1:
+        case 1: // Normal
             G_linhas = 10;
             G_colunas = 10;
             G_minas = 15;
             G_minasatuais = 15;
             break;
-        case 2:
+        case 2: // Hard
             G_linhas = 15;
             G_colunas = 15;
             G_minas = 30;
             G_minasatuais = 30;
             break;
-        case 0:
+        case 0: // Easy
         default:
             G_linhas = 5;
             G_colunas = 5;
@@ -139,7 +147,8 @@ void MenuTemporario(){
     }
 }
 
-static void renderText(const char *text, int length, int x, int y){ // Função para renderizar o texto
+static void renderText(const char *text, int length, int x, int y)
+{// Função para renderizar o texto
     glMatrixMode(GL_PROJECTION);
     double *matrix = new double[16];
     glGetDoublev(GL_PROJECTION_MATRIX, matrix);
@@ -160,7 +169,8 @@ static void renderText(const char *text, int length, int x, int y){ // Função pa
     glFlush();
 }
 
-static void mostraTempo(int value){                                 //Função para mostrar o tempo decorrido
+static void mostraTempo(int value)
+{// Função para mostrar o tempo decorrido
     std::string text;
     G_timer = glutGet(GLUT_ELAPSED_TIME);
     text = to_string(G_timer/1000); //Converte o G_timer para uma string
@@ -192,7 +202,8 @@ static void mostraMinas()
     renderText(text.data(), text.size(), 63, 2);
 }
 
-static void Menus(){
+static void Menus()
+{// Cria os campos dos Menus
     glBegin(GL_LINE_LOOP);
         glVertex2f(0, 0);
         glVertex2f(0, 1);
@@ -201,7 +212,8 @@ static void Menus(){
     glEnd();
 }
 
-static void Limpa(){
+static void Limpa()
+{//
     glBegin(GL_QUADS);
         glVertex2f(0, 0);
         glVertex2f(0, 1);
@@ -263,41 +275,41 @@ static void Bandeira()
 }
 
 static void Tabuleiro()
-{
+{// Funcao que gera o Tabuleiro
     float posStart_x = (float(G_colunas) / 2) * (-1);       // posicao x de onde comecar a desenhar
     float posStart_y = (float(G_linhas) / 2) * (-1);        // posicao y de onde comecar a desenhar
 
-    int linha = 0;                                          // iterar nas linhas
-    int coluna = 0;                                         // iterar nas colunas
+    int linha = 0;                                          // iteradorr das linhas
+    int coluna = 0;                                         // iteradorr das colunas
 
     int cont = 0;                                           // contador para indicar o indice do campo
 
     glTranslatef(posStart_x, posStart_y, 0.0);              // translada para as posicoes X e Y iniciais do Tabuleiro
     glPushMatrix();
-    for(linha = 0; linha < G_linhas; linha++)
+    for(linha = 0; linha < G_linhas; linha++)               // iterar nas linhas
     {
         glPushMatrix();
-        for(coluna = 0; coluna < G_colunas; coluna++)
+        for(coluna = 0; coluna < G_colunas; coluna++)       // iterar nas colunas
         {
             Calculo_Desenho(linha, coluna, cont);           // determinar o que desenhar, campo nao revelado, campo revelado ou uma mina
             cont++;                                         // incrementa o contador do indice;
-            glTranslatef(1.0, 0.0, 0.0);                    // desloca em x para posicionar a proxima coluna
+            glTranslatef(1.0, 0.0, 0.0);                    // translada em x para posicionar a proxima coluna
         }
-        glPopMatrix();                                      // retorna ao X inicial
-        glTranslatef(0.0, 1.0, 0.0);                        // desloca em y para posicionar a proxima linha
+        glPopMatrix();                                      // retorna a posicao X inicial
+        glTranslatef(0.0, 1.0, 0.0);                        // translada em y para posicionar a proxima linha
     }
-    glPopMatrix();                                          // retorna ao Y inicial
+    glPopMatrix();                                          // retorna a posicao Y inicial
 }
 
-static void Calculo_Desenho(int linha, int coluna, int indice)   // Funcao para determinar o que desenhar, com base no clique do Mouse realizado
-{
+static void Calculo_Desenho(int linha, int coluna, int indice)
+{// Funcao para determinar o que desenhar, com base no clique do Mouse realizado
     int operador = Calculo_Posicao(linha, coluna) ? G_operacao_desenho : 0;
     // operador ternario,
     //se o clique corresponder ao campo da interacao do atual do Tabuleiro, fazer operacoes do clqiue do mouse (revelar um campo, operador = 1, ou colocar/retirar a protecao(bandeira), operador = 2))
     //senao apenas mostrar as proprieades dos campos (operador = 0)
     switch(operador)
     {
-        case 0:                                             // apenas mostrar o campo conforme sua propriedade, Campo Revelado, Campo Protegido, Campo Mina ou Campo nao Revelado
+        case 0:                                                         // apenas mostrar o campo conforme sua propriedade, Campo Revelado, Campo Protegido, Campo Mina ou Campo nao Revelado
             if(campo_minado[indice].revelado)
             {
                 Revelado();
@@ -306,7 +318,7 @@ static void Calculo_Desenho(int linha, int coluna, int indice)   // Funcao para 
             {
                 Bandeira();
             }
-            else if(campo_minado[indice].campo_mina)        // Condicao com fins de DEBUG
+            else if(campo_minado[indice].campo_mina)                    // Condicao para fins de DEBUG
             {
                 Mina();
             }
@@ -315,21 +327,22 @@ static void Calculo_Desenho(int linha, int coluna, int indice)   // Funcao para 
                 Quadrado();
             }
             break;
-        case 1:                                             // Funcao para revelar um campo
+        case 1:                                                         // Funcao para revelar um campo
             Revelar_Campo(indice);
             break;
-        case 2:                                             // Funcao para alternar a propriedade de Protegido
+        case 2:                                                         // Funcao para alternar a propriedade de Protegido
             Alternar_Protecao(indice);
             break;
     }
 }
 
-static bool Calculo_Posicao(int linha, int coluna)                                  // Calcula se o local clicado, corresponde ao campo da iteração na funcao Tabuleiro
-{
-    float coluna_minimo = coluna * (windowsSize_x * 0.05);                          // 0.05: relembrar que o quadrado tera 5% do tamanho da tela
-    float coluna_maximo = coluna * (windowsSize_x * 0.05) + (windowsSize_y * 0.05);
-    float linha_minimo = linha * (windowsSize_y * 0.05);
-    float linha_maximo = linha * (windowsSize_y * 0.05) + (windowsSize_y * 0.05);
+static bool Calculo_Posicao(int linha, int coluna)
+{// Calcula se o local clicado, corresponde ao campo da iteração na funcao Tabuleiro
+    float coluna_minimo = coluna * (windowsSize_x * 0.05);                          // representa o "pixel" minimo no eixo x
+    float coluna_maximo = coluna * (windowsSize_x * 0.05) + (windowsSize_y * 0.05); // representa o "pixel" maximo no eixo x, nada mais que o minimo + tamanho do campo (5% do tamanho da tela)
+    float linha_minimo = linha * (windowsSize_y * 0.05);                            // representa o "pixel" minimo do eixo y
+    float linha_maximo = linha * (windowsSize_y * 0.05) + (windowsSize_y * 0.05);   // representa o "pixel" maximo do eixo y, nada mais que o minimo + tamanho do campo (5% do tamanho da tela)
+    // 0.05: relembrar que o quadrado tera 5% do tamanho da tela
     if(G_timer != 0)                                                                // if temporario para que nao revele logo no inicio algum campo
     {
         if((G_click_pos_x > coluna_minimo) && (G_click_pos_x < coluna_maximo))          // Verificao do clique no eixo X
@@ -344,7 +357,7 @@ static bool Calculo_Posicao(int linha, int coluna)                              
 }
 
 static void Revelar_Campo(int indice)
-{
+{// Funcao para revelar um campo
     if(campo_minado[indice].revelado)                   // Campo ja revelado ?
     {
         Revelado();
@@ -379,22 +392,22 @@ static void Revelar_Campo(int indice)
 }
 
 static void Alternar_Protecao(int indice)
-{
-    if(campo_minado[indice].protegido)
+{// Funcao para alternar a protecao
+    if(campo_minado[indice].protegido)                  // Campo protegido ?
     {
         Quadrado();
         // Incrementar o numero de Bandeiras disponiveis
         campo_minado[indice].protegido = false;
     }
-    else
+    else                                                // Campo nao protegido
     {
-        if(campo_minado[indice].revelado)
+        if(campo_minado[indice].revelado)               // Campo revelado ?
         {
             Revelado();
             // Mostrar Numeros Adjacentes
             printf("\n     Campo ja revelado.");
         }
-        else
+        else                                            // Campo nao revelado
         {
             Bandeira();
             // Decrementar o numero de Bandeiras disponiveis
@@ -402,6 +415,7 @@ static void Alternar_Protecao(int indice)
         }
     }
 }
+
 
 static void Atualiza_tamanho(int largura, int altura){
     glViewport(0, 0, largura, altura);
@@ -448,6 +462,7 @@ static void Atualiza_desenho(void){
 
     //printf("\n[DEBUG] : Evento Atualiza desenho\n");
 }
+
 
 static void teclado(unsigned char tecla, int x, int y){
     switch (tecla)
@@ -497,6 +512,7 @@ static void mouse(int botao, int estado, int x, int y){
         printf("\n[DEBUG]: Nao entendo o que vc quer clicar, pare de apertar esse botao por favor");
     }
 }
+
 
 int main(){
     MenuTemporario();
